1f955d6154dd2eaaccf7c283381d319b35db653a
Satya Sharma <83288606+sattya19@users.noreply.github.com>
2023-03-17 20:21:52 +0530

    Add files via upload

---
+++ activity feed/index.js
@@ -0,0 +1,8 @@
+function show_hide() {
+    var x = document.getElementById("feed");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
